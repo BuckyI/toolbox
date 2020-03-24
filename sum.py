6ebbecafe0376f12c49f68858d4477873bc1ea3d
BuckyI <44031088+BuckyI@users.noreply.github.com>
2020-03-24 19:13:25 +0800

    增加了毛概功能！
    另外get到了更换到父目录的方法

--- sum.py
+++ sum.py
@@ -5,14 +5,20 @@ import os
 
 info = input("""
         选择功能：
-        1. 直接回车使用clipboard
-        2. 输入东西查看密码
+        1. 输入1使用clipboard
+        2. 输入2查看密码
+        3. 输入3毛概收集
         """)
 
-if info:
+if info == "2":
     os.system("python ../xlsx/xlsx.py")
-else:
+elif info == "1":
     os.system("python ../master_clipboard/clipboard.py")
+elif info == "3":
+    # 切换工作目录
+    os.chdir(os.path.abspath(os.path.join(os.getcwd(), "..", "maogai")))
+    os.system("dir")
+    os.system("python exe.py")
 
 # 执行结束，暂停
 print("程序执行结束")
