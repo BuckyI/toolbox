5d8a46f8f6b065f96429af000f6df2648c25755a
BuckyI <44031088+BuckyI@users.noreply.github.com>
2020-03-31 00:53:52 +0800

    添加了新功能

--- sum.py
+++ sum.py
@@ -8,6 +8,7 @@ info = input("""
         1. 输入1使用clipboard
         2. 输入2查看密码
         3. 输入3毛概收集
+        4. 输入4markdown添加序号
         """)
 
 if info == "2":
@@ -15,9 +16,11 @@ if info == "2":
 elif info == "1":
     os.system("python ../master_clipboard/clipboard.py")
 elif info == "3":
-    # 切换工作目录
+    # 切换工作目录，因为这个脚本里面也用到了工作目录
     os.chdir(os.path.abspath(os.path.join(os.getcwd(), "..", "maogai")))
     os.system("python exe.py")
+elif info == "4":
+    os.system("python ../markdown_process/mp.py")
 
 # 执行结束，暂停
 print("程序执行结束")
