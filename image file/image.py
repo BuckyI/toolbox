59c93008986bfbedac29b939988e84a3774aa2a1
BuckyI <454897555@qq.com>
2023-02-05 20:15:03 +0800

    ✨ feat(image.py): Image 修正文件创建时间
    根据文件名、文件原本的时间信息，选择出合适的最早的时间作为文件创建时间。

--- image file/image.py
+++ image file/image.py
@@ -5,6 +5,7 @@ import logging
 import time
 import re
 import uuid
+from win32_setctime import setctime
 
 
 class Image():
@@ -35,8 +36,7 @@ class Image():
             if m := re.match(r"20\d{6}", tag):
                 # 日期信息，已标注的优先级较高，可作为文件日期
                 # TODO: 这里可以添加一个识别日期的 class，多添加需要的模式
-                # TODO: 根据文件名识别日期，修改文件本身的日期
-                pass
+                self.set_time_status(m.group())  # 读取的日期参与设定
             elif re.match(r"[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}", tag):
                 # uuid
                 pass
@@ -83,6 +83,37 @@ class Image():
             self.path = scr.rename(dst)  # 进行重命名并对存储的路径进行跟踪
             logging.info(f"[RENAME] from \"{scr.name}\" to \"{dst.name}\"")
 
+    def set_time_status(self, set_time: str, format="%Y%m%d"):
+        """change file time status, it compares folowing three times
+        set the create time with the earlist proper one
+        - input set_time
+        - os.stat mtime
+        - os.stat ctime
+        """
+        def str_time(t):
+            return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
+
+        try:
+            st_stime = time.strptime(set_time, format)
+            stime = time.mktime(st_stime)
+        except OverflowError:
+            logging.error("输入的时间错误 %s", set_time)
+            return
+
+        # 候选人
+        times = [self.path.stat().st_ctime, self.path.stat().st_mtime, stime]
+        # 由于输入字符串只有年月日，默认的时分秒为0，要避免因此而误调整
+        st_times = [time.localtime(t)[0:3] for t in times]  # 计算出年月日
+        if st_times[0] == st_times[2] or st_times[1] == st_times[2]:
+            times.pop()
+        # 找到最远的时间
+        i = times.index(min(times))
+        if i != 0:
+            setctime(self.path.absolute(), times[i])
+            logging.info("[cTime change] file '%s' from %s to %s",
+                         self.path.name, str_time(times[0]),
+                         str_time(times[i]))
+
 
 def scan(path: str, recursive=False):
     """scan all image files in path (recursicely)"""
@@ -99,13 +130,23 @@ def scan(path: str, recursive=False):
 
 def add_handler():
     "部分图片处理 log 输出到本地文件"
+
+    def match(s):
+        msg = s.msg
+        mode = ["[RENAME]", "[cTime change]"]
+        for m in mode:
+            if m in msg:
+                return True
+        else:
+            return False
+
     name = "Image Info to File Log"
     if name in [h.name for h in logging.root.handlers]:
         return  # 已经存在了就不重复添加
     handler = logging.FileHandler("images_change.log")
     handler.set_name(name)
     handler.setLevel(logging.INFO)
-    handler.addFilter(lambda s: "[RENAME]" in s.msg)  # msg 含 RENAME 时，保存到文件
+    handler.addFilter(match)
     handler.setFormatter(logging.Formatter('%(asctime)s : %(message)s'))
     logging.root.addHandler(handler)  # add handler to root logger
     logging.debug("add log handler '%s' to '%s'", handler.name,
