520253e2ee0f1c3b336c0ed098bb57bf741c2024
BuckyI <454897555@qq.com>
2023-02-25 16:13:45 +0800

    ✨ feat(window.py): 批量 build

--- Markdown/window.py
+++ Markdown/window.py
@@ -20,6 +20,7 @@ class Window():
         self.ui.select_source.clicked.connect(lambda: self.set_folder(0))
         self.ui.select_dest.clicked.connect(lambda: self.set_folder(1))
         self.ui.buildButton.clicked.connect(self.build)
+        self.ui.batchbuildButton.clicked.connect(self.batch_build)
 
         self.ui.show()
         app.exec_()
@@ -54,6 +55,31 @@ class Window():
             my.build()
             QMessageBox(QMessageBox.Information, "Success", "Build completed.",
                         QMessageBox.Ok).exec_()
+
+    def batch_build(self):
+        """
+        source 下面的文件夹依次是文档文件夹，在 dest 下创建同名文件夹存放 html
+        """
+        for i in os.scandir(self.ui.source.text()):
+            if not i.is_dir() or i.name.startswith(".") or i.name == "assets":
+                continue
+            source = Path(i)
+            dest = Path(self.ui.destination.text()) / source.name
+            if not dest.exists():
+                dest.mkdir()
+            # Show the message box and get the user's response
+            msg = str(source) + "\n------>\n" + str(dest)
+            msgBox = QMessageBox(QMessageBox.Question, "Confirm Paths", msg,
+                                 QMessageBox.Yes | QMessageBox.No)
+            reply = msgBox.exec_()
+            # Check if the user clicked yes
+            if reply == QMessageBox.Yes:
+                my = mkdocsYAML(site_name=source.name,
+                                site_dir=dest,
+                                docs_dir=source)
+                my.build()
+        QMessageBox(QMessageBox.Information, "Success", "Build all completed.",
+                    QMessageBox.Ok).exec_()
 
 
 if __name__ == "__main__":
