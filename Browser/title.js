422e0a6dca5cc7b7b8806e55b5ff76e86c0c0128
BuckyI <454897555@qq.com>
2023-03-29 23:02:25 +0800

    🌈 style(title.js): 空格数

--- Browser/title.js
+++ Browser/title.js
@@ -19,15 +19,15 @@
     var loopCount = 0;
     var intervalId = setInterval(function () {
         loopCount++;
-        if (loopCount >= 10) {  // 循环达到10s后自动退出
+        if (loopCount >= 10) { // 循环达到10s后自动退出
             clearInterval(intervalId);
             console.log('已超时，未发现标题变化');
             return;
         }
-        if (document.title !== originalTitle) {  // 标题已经被修改
+        if (document.title !== originalTitle) { // 标题已经被修改
             document.title = originalTitle;
             clearInterval(intervalId);
             console.log('标题已恢复为原始标题：', originalTitle);
         }
-    }, 1000);  // 每隔1秒执行一次循环
+    }, 1000); // 每隔1秒执行一次循环
 })();
