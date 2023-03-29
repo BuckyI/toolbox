15fc4d64c915447ad2000d982217c80d0ee827aa
BuckyI <454897555@qq.com>
2023-03-29 22:56:32 +0800

    ✨ feat(title.js): 改进策略
    之前是等待5s后，认为标题已经被修改了。这次是循环每隔1s进行判断。

--- Browser/title.js
+++ Browser/title.js
@@ -15,9 +15,19 @@
 
     // 读取当前页面的标题
     var originalTitle = document.title;
-    setTimeout(function () {
-        document.title = originalTitle;
-        // 输出调试信息
-        console.log('原始标题：', originalTitle);
-    }, 5000); // 5 秒后执行
+
+    var loopCount = 0;
+    var intervalId = setInterval(function () {
+        loopCount++;
+        if (loopCount >= 10) {  // 循环达到10s后自动退出
+            clearInterval(intervalId);
+            console.log('已超时，未发现标题变化');
+            return;
+        }
+        if (document.title !== originalTitle) {  // 标题已经被修改
+            document.title = originalTitle;
+            clearInterval(intervalId);
+            console.log('标题已恢复为原始标题：', originalTitle);
+        }
+    }, 1000);  // 每隔1秒执行一次循环
 })();
