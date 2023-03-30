1de77a7adde065cea4630de9c96878904040cd39
BuckyI <454897555@qq.com>
2023-03-30 22:42:52 +0800

    ✨ feat(title.js): 使用异步操作进行

--- Browser/title.js
+++ Browser/title.js
@@ -1,38 +1,45 @@
 // ==UserScript==
 // @name         Mightymjolnir's Script
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        *://*/*
 // @run-at       document-body
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=theb.ai
 // @grant        none
+// TODO 清洁标题的垃圾信息，一大串后缀之类的
 // ==/UserScript==
 
-(function () {
-    'use strict';
+function waitForTitleChange(originalTitle, timeout) {
+    return new Promise((resolve, reject) => {
+        const observer = new MutationObserver((mutationsList, observer) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'childList' && document.title !== originalTitle) {
+                    document.title = originalTitle;
+                    observer.disconnect();
+                    resolve(document.title);
+                    return;
+                }
+            }
+        });
 
-    // 读取当前页面的标题
-    var originalTitle = document.title;
-    // 如果标题为空，则一直读取
-    while (originalTitle === '') {
-        originalTitle = document.title;
+        observer.observe(document.querySelector('head'), { childList: true });
+
+        setTimeout(() => {
+            observer.disconnect();
+            reject(new Error('时间到了，标题应该不会被修改了吧'));
+        }, timeout);
+    });
+}
+
+(async function () {
+    try {
+        const originalTitle = document.title;
+        console.log('读取到原标题', originalTitle);
+        const title = await waitForTitleChange(originalTitle, 10000);
+        console.log('读取到变更标题 ', title, '，已恢复原标题');
+    } catch (error) {
+        console.error(error.message);
     }
-    console.log('读取到标题：', originalTitle);
-
-    var loopCount = 0;
-    var intervalId = setInterval(function () {
-        loopCount++;
-        if (loopCount >= 10) { // 循环达到10s后自动退出
-            clearInterval(intervalId);
-            console.log('已超时，未发现标题变化');
-            return;
-        }
-        if (document.title !== originalTitle) { // 标题已经被修改
-            console.log('读取到变更标题，已恢复原标题', document.title);
-            document.title = originalTitle;
-            clearInterval(intervalId);
-        }
-    }, 1000); // 每隔1秒执行一次循环
 })();
