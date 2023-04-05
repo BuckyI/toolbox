3e375cdbcffd54c18b993557b731cece319522c0
BuckyI <454897555@qq.com>
2023-04-05 18:51:07 +0800

    🦄 refactor(copy_metadata): 脚本变成可被调用的函数

--- ImageKit/copy_metadata.py
+++ ImageKit/copy_metadata.py
@@ -8,30 +8,36 @@ from win32file import GENERIC_READ, GENERIC_WRITE, OPEN_EXISTING
 from pywintypes import Time
 # import time
 
-scr = "source/"
-dest = "dest/"
 
-# link images in scr and dest that have same name
-scr_files = [f for f in os.scandir(scr) if f.is_file()]
-scr_filenames = [f.name for f in scr_files]
-dest_files = [f for f in os.scandir(dest) if f.is_file()]
-scr_dest_pairs = []
-for f in dest_files:
-    try:
-        i = scr_filenames.index(f.name)
-        scr_dest_pair = (scr_files[i], f)
-        scr_dest_pairs.append(scr_dest_pair)
-    except ValueError:
-        print(f"{f.name} dont have a source file.")
+def copy_metadata(scr, dest):
+    # link images in scr and dest that have same name
+    scr_files = [f for f in os.scandir(scr) if f.is_file()]
+    scr_filenames = [f.name for f in scr_files]
+    dest_files = [f for f in os.scandir(dest) if f.is_file()]
+    scr_dest_pairs = []
+    for f in dest_files:
+        try:
+            i = scr_filenames.index(f.name)
+            scr_dest_pair = (scr_files[i], f)
+            scr_dest_pairs.append(scr_dest_pair)
+        except ValueError:
+            print(f"{f.name} dont have a source file.")
 
-# copy time metadata
-for s, d in scr_dest_pairs:
-    scr_fh = CreateFile(s.path, GENERIC_READ, 0, None, OPEN_EXISTING, 0, 0)
-    dest_fh = CreateFile(d.path, GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, 0)
-    createTimes, accessTimes, modifyTimes = GetFileTime(scr_fh)
-    # 有时候复制文件等操作会刷新创建时间，这里修改一下，让创建时间是最早的
-    if createTimes > modifyTimes:
-        createTimes = modifyTimes
-    SetFileTime(dest_fh, createTimes, accessTimes, modifyTimes)
-    CloseHandle(scr_fh)
-    CloseHandle(dest_fh)
+    # copy time metadata
+    for s, d in scr_dest_pairs:
+        scr_fh = CreateFile(s.path, GENERIC_READ, 0, None, OPEN_EXISTING, 0, 0)
+        dest_fh = CreateFile(d.path, GENERIC_WRITE, 0, None, OPEN_EXISTING, 0,
+                             0)
+        createTimes, accessTimes, modifyTimes = GetFileTime(scr_fh)
+        # 有时候复制文件等操作会刷新创建时间，这里修改一下，让创建时间是最早的
+        if createTimes > modifyTimes:
+            createTimes = modifyTimes
+        SetFileTime(dest_fh, createTimes, accessTimes, modifyTimes)
+        CloseHandle(scr_fh)
+        CloseHandle(dest_fh)
+
+
+if __name__ == '__main__':
+    scr = "source/"
+    dest = "dest/"
+    copy_metadata(scr, dest)
