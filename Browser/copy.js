1972690931db9309e1711ea9584c1c3b35368d83
BuckyI <454897555@qq.com>
2023-04-08 22:13:55 +0800

    ✨ feat(copy.js): 复制的同时保留源格式
    借助 chatGPT，思路是转化为 html，然后加一行，然后再放到剪贴板里复制。

--- Browser/copy.js
+++ Browser/copy.js
@@ -5,7 +5,6 @@
 // @description  在复制网页内容后，剪贴板内添加一行网页链接（会丢失格式）
 // @author       You
 // @match        *://*/*
-// @icon         
 // @grant        none
 // ==/UserScript==
 
@@ -13,12 +12,30 @@
     'use strict';
 
     document.addEventListener('copy', function (e) {
-        var selectionText = document.getSelection().toString();
-        var pagelink = '-- [source](' + document.location.href + ')';
-        var copyText = selectionText + '\r\n' + pagelink;
-        e.clipboardData.setData('text/plain', copyText);
-        e.preventDefault();
+        var selection = window.getSelection();
+        if (selection.rangeCount) {
+            // create a new div element to contain the selected text and link
+            var container = document.createElement('div');
+
+            // get the selected range and clone it
+            var range = selection.getRangeAt(0);
+            var clonedRange = range.cloneRange();
+
+            // wrap the cloned range in the new container div
+            container.appendChild(clonedRange.cloneContents());
+
+            // add the source link to the end of the copied text
+            var pagelink = '<br/>-- [' + document.title + '](' + document.location.href + ')';
+            container.innerHTML += pagelink;
+
+            // serialize the HTML contents of the container div
+            var serializedHtml = (new XMLSerializer()).serializeToString(container);
+
+            // set the serialized HTML as the data to be copied to the clipboard
+            e.clipboardData.setData('text/html', serializedHtml);
+
+            // prevent the default copy behavior
+            e.preventDefault();
+        }
     });
 })();
-
-
