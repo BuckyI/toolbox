7fdf73d9f81569a9d5d3c2c236108d40aac1cfcc
BuckyI <454897555@qq.com>
2023-04-14 14:10:28 +0800

    ✨ feat(copy.js): 增加停止功能的选项

--- Browser/copy.js
+++ Browser/copy.js
@@ -8,43 +8,44 @@
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
 
-function addEvent() {
-    document.addEventListener('copy', function (e) {
-        var selection = window.getSelection();
-        if (selection.rangeCount) {
-            // create a new div element to contain the selected text and link
-            var container = document.createElement('div');
+function processClipboard(e) {
+    var selection = window.getSelection();
+    if (selection.rangeCount) {
+        // create a new div element to contain the selected text and link
+        var container = document.createElement('div');
 
-            // get the selected range and clone it
-            var range = selection.getRangeAt(0);
-            var clonedRange = range.cloneRange();
+        // get the selected range and clone it
+        var range = selection.getRangeAt(0);
+        var clonedRange = range.cloneRange();
 
-            // wrap the cloned range in the new container div
-            container.appendChild(clonedRange.cloneContents());
+        // wrap the cloned range in the new container div
+        container.appendChild(clonedRange.cloneContents());
 
-            // add the source link to the end of the copied text
-            var pagelink = '<br/>-- [' + document.title + '](' + document.location.href + ')';
-            container.innerHTML += pagelink;
+        // add the source link to the end of the copied text
+        var pagelink = '<br/>-- [' + document.title + '](' + document.location.href + ')';
+        container.innerHTML += pagelink;
 
-            // serialize the HTML contents of the container div
-            var serializedHtml = (new XMLSerializer()).serializeToString(container);
+        // serialize the HTML contents of the container div
+        var serializedHtml = (new XMLSerializer()).serializeToString(container);
 
-            // set the serialized HTML as the data to be copied to the clipboard
-            e.clipboardData.setData('text/html', serializedHtml);
+        // set the serialized HTML as the data to be copied to the clipboard
+        e.clipboardData.setData('text/html', serializedHtml);
 
-            // prevent the default copy behavior
-            e.preventDefault();
-        }
-    });
+        // prevent the default copy behavior
+        e.preventDefault();
+    }
 }
 
 
 (function () {
     'use strict';
-    // addEvent();
     // 注册快捷键并关联回调函数
     GM_registerMenuCommand("复制添加网页链接", function () {
-        addEvent();
+        document.addEventListener('copy', processClipboard);
         alert('现在开始，复制文字都会添加网页链接！');
+    });
+    GM_registerMenuCommand("停用", function () {
+        document.removeEventListener('copy', processClipboard);
+        alert('已停用 EventListener ');
     });
 })();
